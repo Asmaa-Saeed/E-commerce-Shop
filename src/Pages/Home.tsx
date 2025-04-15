a696990c95c511e6c30c3a4e8f875c56aa0f9b6d
asmaa-saeed <asmaasaeed2082003@google.com>
2025-04-15 12:39:56 +0200

    style(Home): format code and remove commented import
    Refactor the Home component to improve readability by removing unnecessary comments and fixing indentation.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,19 +1,19 @@
 import Footer from "../Layouts/Footer";
+// import Navbar from "../ Layouts / Navbar";
 import HeroSection from "../Sections/home-sections/HeroSection";
 import ProductSection from "../Sections/home-sections/Products";
 import ProductsCategories from "../Sections/home-sections/ProductsCategories";
 
 const Home = () => {
     return (
-        <div>
-           <HeroSection />
-           <ProductsCategories/>
-           <ProductSection/>
-           {/* <CoverflowSwiper/> */}
-           <Footer/>
-       </div>
+       <div>
 
+          <HeroSection/>
+          <ProductsCategories/>
+          <ProductSection/>
+          <Footer/>
+       </div> 
     )
-}
+}  
 
 export default Home;
